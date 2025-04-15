7496518f210b2798ba623ad7d400fc8103cc85dc
benkhedda4 <benkheddaa125@gmail.com>
2025-04-16 01:46:23 +0200

    initial Push

--- src/components/heroSection/HeroPic.jsx
+++ src/components/heroSection/HeroPic.jsx
@@ -12,7 +12,7 @@ const HeroPic = () => {
       className=" h-full flex items-center justify-center "
     >
       <img
-        src="../../public/images/HexaPic.png"
+        src="/Ahmed-benkheda/images/HexaPic.png"
         alt="Ahmed Benkhedda"
         className="max-h-[575px] w-auto"
       />
